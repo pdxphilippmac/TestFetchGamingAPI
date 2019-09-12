5115e33c4e26a5ad042ca4b7bbfa0b32a5501f9b
Philipp Maecke <p.maecke@gmx.de>
2019-09-12 22:24:01 +0200

    add dependencies apicalypse etc

--- gaming-api/src/App.js
+++ gaming-api/src/App.js
@@ -1,6 +1,14 @@
 import React from "react";
+import igdb from "igdb-api-node";
 
 import "./App.css";
+const client = igdb("e2715f17601c1d968b592f747c6aa839");
+
+const response = await apicalypse(rawQueryString, {
+  queryMethod: "url"
+}).request("https://myapi.com/actors/nm0000216");
+
+console.log(response.data);
 
 function App() {
   return <div className="App">test</div>;
